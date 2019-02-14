13e7b60c152420e1a36f82d62b819e1cf7c6c42e
Will Beal <willbeal@outlook.com>
2019-02-14 14:21:55 +0100

    Add project files.

---
+++ SyncfusionTest/SyncfusionTest/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+
+namespace SyncfusionTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NjA0NjhAMzEzNjJlMzQyZTMwWXJKMlVOYlRpNmJxdVJaZHZueTZ0Z01DTG9Qam1Ydk1xMnhmRlNpdUUxRT0=");
+
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
